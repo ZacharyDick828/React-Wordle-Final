3269dd8b3757b53281da227aff648256e77afd18
ZacharyDick828 <91510605+ZacharyDick828@users.noreply.github.com>
2022-06-29 16:23:26 -0700

    testing random word generator from json db

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json())
+    .then(json => {
+      // console.log(json)
+      const randomWord = json[Math.floor(Math.random()*json.length)]
+      setSolution(randomWord.word)
+    })
+  }, [setSolution])
   return (
     <div className="App">
       <h1>Wordle (Comics)</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
