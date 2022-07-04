160fe77ff1df915b1b5ce424a5a1380e57e8c184
ZacharyDick828 <91510605+ZacharyDick828@users.noreply.github.com>
2022-07-04 15:43:37 -0700

    created an end-game component that delivers the client a message

---
+++ src/components/Endgame.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Endgame({ isCorrect, turn, solution }) {
+    return (
+        <div className='endgame'>
+            {isCorrect && (
+                <div>
+                    <h1> WINNERRRRRRR!!!!!!! YOU'RE A NERD GET SOME FRIENDS! </h1>
+                    <p className='solution'>{solution}</p>
+                    <p>YOU FOUND THE SOLUTION IN {turn} TURNS!</p>
+                </div>
+            )}
+            {!isCorrect && (
+                <div>
+                    <h1>YOU SUCK! YOU'LL NEVER BEAT MY GAME</h1>
+                    <p className='solution'>The Solution Was {solution}</p>
+                </div>
+            )}
+        </div>
+    )
+}
--- src/components/Worlde.js
+++ src/components/Worlde.js
@@ -1,26 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Endgame from './Endgame'
 
 export default function Wordle({ solution }) {
 
     const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } = useWordle(solution)
+    const [showEndgame, setShowEndgame] = useState(false)
 
     useEffect (() => {
         window.addEventListener('keyup', handleKeyup)
 
-        return () => window.removeEventListener('keyup', handleKeyup)
-    }, [handleKeyup])
+        if (isCorrect) {
+            console.log('YOU WIN! HIT REFRESH')
+            setTimeout(() => setShowEndgame(true), 2000)
+            window.removeEventListener('keyup', handleKeyup)
+        }
+        if(turn > 5) {
+            console.log('you lose! no more guesses')
+            setTimeout(() => setShowEndgame(true), 2000)
+            window.removeEventListener('keyup', handleKeyup)
+        }
 
-    useEffect(() => {
-        console.log(guesses, turn, isCorrect)
-    }, [guesses, turn, isCorrect])
+        return () => window.removeEventListener('keyup', handleKeyup)
+    }, [handleKeyup, isCorrect, turn])
 
     return (
     <>      
     <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
     <Keypad usedKeys={usedKeys}/>
+    {showEndgame && <Endgame  isCorrect={isCorrect} turn={turn} solution={solution}/>}
 </>
     )
 }
