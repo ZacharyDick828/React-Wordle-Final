5e094da83abb901ae0488415fb6ae850449635ef
ZacharyDick828 <91510605+ZacharyDick828@users.noreply.github.com>
2022-07-06 17:50:29 -0700

    changed endpoints to match server

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,7 @@ export default function Keypad({ usedKeys }) {
   const [letters, setLetters] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3001/letters')
+    fetch('https://desolate-sea-85910.herokuapp.com/letters')
       .then(res => res.json())
       .then(json => {
         setLetters(json)
